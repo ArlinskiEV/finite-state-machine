22584bfeb288085d37c87c5b6ebf471b08fb70d0
ArlinskiEV <ArlinskiEV@gmail.com>
2017-09-29 13:43:20 +0300

    firs solution is bad

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,57 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+
+    constructor(config) {
+      this.config = config;
+      this.states = [];
+      this.states.unshift(config.initial);
+      this.state = 0;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+      return this.states.length > 0 ? this.states[this.state]:'';
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+      //find state
+      if (this.config.states[state] != null) {
+        while (this.state != 0) {
+          this.states.shift();
+          this.state--;
+        }
+        this.states.unshift(state);
+      }
+      else throw(Error);
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+      if (this.config[this.getState()].transitions[event] != null) {
+        this.changeState(this.config[this.getState()].transitions[event]);
+      } else throw(Error);
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+      this.config = config;
+      this.states = [];
+      this.states.unshift(config.initial);
+      this.state = 0;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -41,19 +68,34 @@ class FSM {
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+      if (this.state < this.states.length) {
+        this.state++;
+        return true;
+      }
+      return false;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+      if (this.state == 0) return false;
+      this.state--;
+      return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+      var temp = this.states[this.state];
+      this.states = [];
+      this.states.unshift(temp);
+      this.state = 0;
+    }
 }
 
 module.exports = FSM;
